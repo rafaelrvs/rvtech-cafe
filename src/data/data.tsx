966c338a79d1a62751c7530e7086d1f51e79f134
rafaelrvs <rafaelvieira1333@gmail.com>
2025-01-06 12:04:39 -0300

    adicionando melhorias google

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -7,31 +7,32 @@ interface Cafe {
 
 export const dataCafe: Cafe[] = [
   {
-    img: "/src/assets/cardCafe.svg",
+   
+    img: "/images/cardCafe.svg",
     nome: "Café Preto",
     preco: 12.2, // Preço formatado como número
     detalhes: "Um café preto clássico.",
   },
   {
-    img: "/src/assets/cardCafe.svg",
+    img: "/images/cardCafe.svg",
     nome: "Latte",
     preco: 15.5,
     detalhes: "Café com leite vaporizado.",
   },
   {
-    img: "/src/assets/cardCafe.svg",
+    img: "/images/cardCafe.svg",
     nome: "Cappuccino",
     preco: 16.0,
     detalhes: "Café com leite e espuma cremosa.",
   },
   {
-    img: "/src/assets/cardCafe.svg",
+    img: "/images/cardCafe.svg",
     nome: "Espresso",
     preco: 10.0,
     detalhes: "Um espresso forte e aromático.",
   },
   {
-    img: "/src/assets/cardCafe.svg",
+    img: "/images/cardCafe.svg",
     nome: "Macchiato",
     preco: 14.5,
     detalhes: "Espresso com uma pequena quantidade de espuma de leite.",
